27f0fc45cb6ca123822208280884c1ee5a6e0310
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-03 10:41:32 +0100

    .

--- 1/script.js
+++ 1/script.js
@@ -1,7 +1,7 @@
 const myElement = React.createElement('h1', { className: 'heading' }, 'Lambda Is AWESOME!');
-const jayneSaysHi = React.createElement('h1', { className: 'heading' }, 'Hi Everyone');
-const petarsElement = React.createElement('button', { className: 'btn' }, ['click me!', ]);
-const myReact = React.createElement('p', { className: 'react' }, 'React Is AWESOME!');
+// const jayneSaysHi = React.createElement('h1', { className: 'heading' }, 'Hi Everyone');
+// const petarsElement = React.createElement('button', { className: 'btn' }, ['click me!', ]);
+// const myReact = React.createElement('p', { className: 'react' }, 'React Is AWESOME!');
 const petarSElement = React.createElement('small', { className: 'small' }, 'nice!');
 
 
@@ -9,13 +9,17 @@ const petarSElement = React.createElement('small', { className: 'small' }, 'nice
 
 // JSX
 const myElementJSX = <h1 className="heading">Lambda Is AWESOME! JSX</h1>;
-
+const house = <div className="box1"><p>Where is your house?</p></div>;
+const jayneSaysHi = <h1 className="heading">Hi Everyone</h1>;
+const petarsElement = <button className= "btn">click me!</button>;
+const myReact = <p className="react">React Is AWESOME! JSX</p>;
+const Danikat = <h1 className="heading">Hello everyone!!</h1>;
 
 
 const appContainer = React.createElement(
     'div', 
     { className: 'App' },
-    [jayneSaysHi, myElementJSX, petarsElement, myReact]
+    [Danikat]
 );
 
 ReactDOM.render(appContainer, document.querySelector('#root'));
--- 2/src/App.js
+++ 2/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+function BasicButton() {
+  return <button>Click Me!</button>
+}
+
 function App() {
   return (
     <div className="App">
-
+      <h1>HELLO</h1>
+      <BasicButton />
     </div>
   );
 }
