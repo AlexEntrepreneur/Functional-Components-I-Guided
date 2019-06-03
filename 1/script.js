401fe3500bc837fc3c4dda39bd197646b5686fa9
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-03 10:00:31 +0100

    add babel

--- 1/script.js
+++ 1/script.js
@@ -1,12 +1,21 @@
 const myElement = React.createElement('h1', { className: 'heading' }, 'Lambda Is AWESOME!');
 const jayneSaysHi = React.createElement('h1', { className: 'heading' }, 'Hi Everyone');
-const petarsElement = React.createElement('button', { className: 'btn' }, 'click me!');
+const petarsElement = React.createElement('button', { className: 'btn' }, ['click me!', ]);
 const myReact = React.createElement('p', { className: 'react' }, 'React Is AWESOME!');
 const petarSElement = React.createElement('small', { className: 'small' }, 'nice!');
+
+
+
+
+// JSX
+const myElementJSX = <h1 className="heading">Lambda Is AWESOME! JSX</h1>;
+
+
+
 const appContainer = React.createElement(
     'div', 
     { className: 'App' },
-    [jayneSaysHi, myElement, petarsElement, myReact]
+    [jayneSaysHi, myElementJSX, petarsElement, myReact]
 );
 
 ReactDOM.render(appContainer, document.querySelector('#root'));
