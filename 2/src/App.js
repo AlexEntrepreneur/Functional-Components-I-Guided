dcfa0095385db930b75bcbccf6c51c46987a748a
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-03 10:52:14 +0100

    .

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
-function BasicButton() {
-  return <button>Click Me!</button>
+function BasicButton(props) {
+  return <button>{ props.text }</button>
+
+}
+
+function SectionSome() {
+  return (
+  <section>
+    <div>
+      <p><strong>Are you still there?</strong></p>
+    </div>
+  </section>
+  )
+}
+
+function GoogleIt() {
+  return <a href="https://www.google.com">Go to Google</a>
 }
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO</h1>
-      <BasicButton />
+      <BasicButton text='Click Me!'/>
+      <SectionSome />
+      <GoogleIt />
     </div>
   );
 }
